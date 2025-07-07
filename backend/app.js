9dc9512bdc3a46452618a5e64faef80e0c425fe5
meet tank <meet7tank9@gmail.com>
2025-07-07 16:51:36 +0530

    Change in CORS

--- backend/app.js
+++ backend/app.js
@@ -16,7 +16,7 @@ const app = express()
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.use(cors({
-    origin: "http://localhost:5173"
+    origin: ["http://localhost:5173", "https://book-heaven-online-book-store.vercel.app"]
 }))
 const compression = require('compression');
 app.use(compression());
