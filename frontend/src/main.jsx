0be7ca24b7d8df4d5cd4bebd8d6fb9c1aaf74749
meet tank <meet7tank9@gmail.com>
2025-07-07 16:58:27 +0530

    Remove strict mode from frontend

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,4 +1,4 @@
-import { StrictMode } from 'react'
+// import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
@@ -7,11 +7,11 @@ import { Provider } from "react-redux"
 import store from './store/index.js'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <Router>
-      <Provider store={store}>
-        <App />
-      </Provider>
-    </Router>
-  </StrictMode>,
+  // <StrictMode>
+  <Router>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </Router>
+  // </StrictMode>,
 )
